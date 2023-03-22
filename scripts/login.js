c8a1e0950d761f3837756d78f1820983598100ce
ctd-frontend <92804884+ctd-frontend@users.noreply.github.com>
2023-03-21 21:58:33 -0300

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -3,7 +3,7 @@ window.addEventListener('load', function () {
     const form = document.forms[0];
     const email = document.querySelector('#inputEmail')
     const password = document.querySelector('#inputPassword')
-    const url = 'https://todo-api.ctd.academy';
+    const url = 'https://todo-api.ctd.academy/v1';
 
 
     /* -------------------------------------------------------------------------- */
